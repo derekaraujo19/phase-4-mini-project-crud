88623a5e9d3483003d5689fb30eefece43b56825
Derek Araujo <derekaraujo19@gmail.com>
2022-09-15 18:22:17 -0400

    lab complete

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,44 @@
+class SpicesController < ApplicationController
+  def index
+    spices = Spice.all
+    render json: spices
+  end
+
+  # def show
+  #   spice = Spice.find_by(id: params[:id])
+  #   render json: spice
+  # rescue ActiveRecord::RecordNotFound
+  #     render json: {error: "Spice not found"}, status: :not_found
+  # end
+
+  def create
+    spice = Spice.create(spice_params)
+    render json: spice, status: :created
+  end
+
+  def update
+    spice = Spice.find_by(id: params[:id])
+    if spice
+      spice.update(spice_params)
+      render json: spice
+    else
+      render json: {error: "Spice not found"}
+    end
+  end
+
+  def destroy
+    spice = Spice.find_by(id: params[:id])
+    if spice
+      spice.destroy
+      render json: {}
+    else
+      render json: {error: "Spice not found"}
+    end
+  end
+
+  private
+  def spice_params
+    params.permit(:title, :image, :description, :notes, :rating)
+  end
+
+end
